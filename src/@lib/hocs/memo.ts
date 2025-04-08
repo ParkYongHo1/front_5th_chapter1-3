a1672c51bbceb4a35a56b103d1b8953ee9833328
ParkYongHo1 <qkaxhf8823@naver.com>
2025-04-08 23:49:36 +0900

    Feat | 기본 과제 완성

--- src/@lib/hocs/memo.ts
+++ src/@lib/hocs/memo.ts
@@ -1,10 +1,20 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { shallowEquals } from "../equalities";
-import { ComponentType } from "react";
+import { ComponentType, createElement } from "react";
+import { useRef } from "../hooks";
 
 export function memo<P extends object>(
   Component: ComponentType<P>,
   _equals = shallowEquals,
 ) {
-  return Component;
+  const MemoComponent = (props: P) => {
+    const propsRef = useRef<P | null>(null);
+    const componentRef = useRef<React.ReactElement | null>(null);
+
+    if (!_equals(propsRef.current, props)) {
+      propsRef.current = props;
+      componentRef.current = createElement(Component, props);
+    }
+    return componentRef.current;
+  };
+  return MemoComponent;
 }
